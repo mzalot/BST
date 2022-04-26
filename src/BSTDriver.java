f459d2ed0c84a6a2ade3cc4fa218c9b68366632a
mzalot <69329263+mzalot@users.noreply.github.com>
2022-04-25 20:04:06 -0600

    I made a driver and found that my put method does not work.

---
+++ src/BSTDriver.java
@@ -0,0 +1,29 @@
+/**
+ BSTDriver
+ The program will create a driver for the Binary Search Tree class
+ Author: Mareks Zeile
+ Collaborator(s):
+ Collaboration:
+ Date: 4/25/22
+ On My Honor, I confirm that I followed all collaboration policy guidelines, and that the work I am submitting is my own: MZ
+ **/
+public class BSTDriver {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        System.out.println("Test the size method(should return 0): " + tree.size());
+        System.out.println("Test the isEmpty method(should return true): " + tree.isEmpty());
+        System.out.println("Test the get method(should return null): " + tree.get("hello"));
+        System.out.println("Test the contains method(should return false): " + tree.contains("hello"));
+        System.out.println("Test the put method");
+        tree.put(1, "Trent");
+        tree.put(2, "August");
+        tree.put(3, "Kevburger");
+        System.out.println("Test the size method(should return 3): " + tree.size());
+        System.out.println("Test the isEmpty method(should return false): " + tree.isEmpty());
+        System.out.println("Test the get method(should return Trent): " + tree.get(1));
+        System.out.println("Test the contains method(should return true): " + tree.contains(2));
+        //System.out.println("Test the min method(should return ): " + tree.min());
+        //System.out.println("Test the max method(should return ): " + tree.max());
+        //test put, size, isempty, get, contains, min, max
+    }
+}
